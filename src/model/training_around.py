ff52195163c281a49bc7a5554a058a0a7afb4de2
micheltulane <michel.tulane@usherbrooke.ca>
2019-10-19 15:40:40 -0400

    some more poor scripting

--- src/model/training_around.py
+++ src/model/training_around.py
@@ -5,6 +5,7 @@ __author__ = "Michel Tulane"
 import os
 import numpy as np
 import pandas as pd
+import feather
 import matplotlib.pyplot as plt
 from datetime import datetime
 
@@ -14,32 +15,37 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow import keras
 from keras import layers
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import   Input, Dense, LSTM, GRU, Embedding, Activation
+from tensorflow.python.keras.layers import   Input, Dense, LSTM, GRU, SimpleRNN, Embedding, Activation, CuDNNLSTM
 from tensorflow.python.keras.optimizers import RMSprop
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard, ReduceLROnPlateau
 
 # Execution variables
 TRAIN_MODEL = True
-# BATCH_SIZE = 256
-# SEQ_LENGTH =
+
 DATA_PATH = "../../data/5min_fetched/"
+TRAINING_LOG_PATH = "../../logs/training/"
+MODEL_CHECKPOINT_PATH = TRAINING_LOG_PATH + "checkpoint/model_checkpoint.keras"
+TENSORBOARD_PATH = TRAINING_LOG_PATH + "/tensorboard"
 
 tf.keras.backend.clear_session()  # Reset notebook state.
 
 
 # Import data
-df = pd.read_excel(os.path.abspath(DATA_PATH + "/train_test_data.xlsx"))
+print("Importing data from disk...")
+# df = pd.read_excel(os.path.abspath(DATA_PATH + "/train_test_data.xlsx"))
+df = feather.read_dataframe(os.path.abspath(DATA_PATH + "/USDT_BTC.feather"))
 
-print(df.shape)
-x_data = df.drop(columns=["day", "time", "USDT_BTC_expected_price_change_15min"])
-y_data = df[["USDT_BTC_expected_price_change_15min"]]
+print("Dataframe shame: {shape}".format(shape=df.shape))
+x_data = df.drop(columns=["day", "time", "hour", "USDT_BTC_expected_price_change_15min", "USDT_BTC_expected_price_change_5min"])
+y_data = df[["USDT_BTC_expected_price_change_5min"]]
+target_names = ["BTC_expected_price_change_5min"]
 print(x_data.shape)
 print(y_data.shape)
 
 x_data_ndarray = x_data.values
 y_data_ndarray = y_data.values
 
-train_split = 0.85
+train_split = 0.9
 num_train = int(train_split * len(x_data_ndarray))
 num_test = len(x_data_ndarray)-num_train
 
@@ -57,7 +63,6 @@ print(num_x_signals)
 num_y_signals = y_data_ndarray.shape[1]
 print(num_y_signals)
 
-
 x_scaler = MinMaxScaler()
 print(x_scaler.fit(x_data_ndarray))
 
@@ -71,10 +76,237 @@ x_test_scaled = x_scaler.transform(x_test)
 y_train_scaled = y_scaler.transform(y_train)
 y_test_scaled = y_scaler.transform(y_test)
 
-print("Min x:", np.min(x_train_scaled))
-print("Max x:", np.max(x_train_scaled))
-print("Min y:", np.min(y_train_scaled))
-print("Max y:", np.max(y_train_scaled))
+print("TRAINING SET SCALED:")
+print("Min x :", np.min(x_train_scaled))
+print("Max x :", np.max(x_train_scaled))
+print("Min y :", np.min(y_train_scaled))
+print("Max y: ", np.max(y_train_scaled))
+print("Shape x: ", x_train_scaled.shape)
+print("Shape y: ", y_train_scaled.shape)
+
+
+print("TEST SET SCALED:")
+print("Min x :", np.min(x_test_scaled))
+print("Max x :", np.max(x_test_scaled))
+print("Min y :", np.min(y_test_scaled))
+print("Max y :", np.max(y_test_scaled))
+print("Shape x: ", x_test_scaled.shape)
+print("Shape y: ", y_test_scaled.shape)
+
+
+def batch_generator(batch_size, sequence_length):
+    """
+    Generator function for creating random batches of training-data.
+    """
+
+    # Infinite loop.
+    while True:
+        # Allocate a new array for the batch of input-signals.
+        x_shape = (batch_size, sequence_length, num_x_signals)
+        x_batch = np.zeros(shape=x_shape, dtype=np.float16)
+
+        # Allocate a new array for the batch of output-signals.
+        y_shape = (batch_size, sequence_length, num_y_signals)
+        y_batch = np.zeros(shape=y_shape, dtype=np.float16)
+
+        # Fill the batch with random sequences of data.
+        for i in range(batch_size):
+            # Get a random start-index.
+            # This points somewhere into the training-data.
+            idx = np.random.randint(num_train - sequence_length)
+
+            # Copy the sequences of data starting at this index.
+            x_batch[i] = x_train_scaled[idx:idx + sequence_length]
+            y_batch[i] = y_train_scaled[idx:idx + sequence_length]
+
+        yield (x_batch, y_batch)
+
+
+batch_size = 1024
+sequence_length = 64
+generator = batch_generator(batch_size=batch_size,
+                            sequence_length=sequence_length)
+
+validation_data = (np.expand_dims(x_test_scaled, axis=0),
+                   np.expand_dims(y_test_scaled, axis=0))
+
+model = Sequential()
+model.add(CuDNNLSTM(units=32,
+                    return_sequences=True,
+                    input_shape=(None, num_x_signals,)))
+model.add(Dense(num_y_signals, activation='sigmoid'))
+
+warmup_steps = 8
+
+
+def loss_mse_warmup(y_true, y_pred):
+    """
+    Calculate the Mean Squared Error between y_true and y_pred,
+    but ignore the beginning "warmup" part of the sequences.
+
+    y_true is the desired output.
+    y_pred is the model's output.
+    """
+
+    # The shape of both input tensors are:
+    # [batch_size, sequence_length, num_y_signals].
+
+    # Ignore the "warmup" parts of the sequences
+    # by taking slices of the tensors.
+    y_true_slice = y_true[:, warmup_steps:, :]
+    y_pred_slice = y_pred[:, warmup_steps:, :]
+
+    # These sliced tensors both have this shape:
+    # [batch_size, sequence_length - warmup_steps, num_y_signals]
+
+    # Calculate the MSE loss for each value in these tensors.
+    # This outputs a 3-rank tensor of the same shape.
+    loss = tf.losses.mean_squared_error(labels=y_true_slice,
+                                        predictions=y_pred_slice)
+
+    # Keras may reduce this across the first axis (the batch)
+    # but the semantics are unclear, so to be sure we use
+    # the loss across the entire tensor, we reduce it to a
+    # single scalar with the mean function.
+    loss_mean = tf.reduce_mean(loss)
+
+    return loss_mean
+
+optimizer = RMSprop(lr=1e-3)
+
+model.compile(loss=loss_mse_warmup, optimizer=optimizer)
+
+model.summary()
+
+callback_checkpoint = ModelCheckpoint(filepath=MODEL_CHECKPOINT_PATH,
+                                      monitor='val_loss',
+                                      verbose=1,
+                                      save_weights_only=True,
+                                      save_best_only=True)
+
+callback_early_stopping = EarlyStopping(monitor='val_loss',
+                                        patience=5, verbose=1)
+
+callback_tensorboard = TensorBoard(log_dir=TENSORBOARD_PATH,
+                                   histogram_freq=0,
+                                   write_graph=False)
+
+callback_reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                                       factor=0.1,
+                                       min_lr=1e-4,
+                                       patience=0,
+                                       verbose=1)
+
+callbacks = [callback_early_stopping,
+             callback_checkpoint,
+             callback_tensorboard,
+             callback_reduce_lr]
+
+if TRAIN_MODEL:
+    model.fit_generator(generator=generator,
+                        epochs=30,
+                        steps_per_epoch=100,
+                        validation_data=validation_data,
+                        callbacks=callbacks)
+
+try:
+    model.load_weights(MODEL_CHECKPOINT_PATH)
+except Exception as error:
+    print("Error trying to load checkpoint.")
+    print(error)
+
+result = model.evaluate(x=np.expand_dims(x_test_scaled, axis=0),
+                        y=np.expand_dims(y_test_scaled, axis=0))
+
+print("loss (test-set):", result)
+
+
+def plot_comparison(start_idx, length=100, train=True):
+    """
+    Plot the predicted and true output-signals.
+
+    :param start_idx: Start-index for the time-series.
+    :param length: Sequence-length to process and plot.
+    :param train: Boolean whether to use training- or test-set.
+    """
+
+    if train:
+        # Use training-data.
+        x = x_train_scaled
+        y_true = y_train
+    else:
+        # Use test-data.
+        x = x_test_scaled
+        y_true = y_test
+
+    # End-index for the sequences.
+    end_idx = start_idx + length
+
+    # Select the sequences from the given start-index and
+    # of the given length.
+    x = x[start_idx:end_idx]
+    y_true = y_true[start_idx:end_idx]
+
+    # Input-signals for the model.
+    x = np.expand_dims(x, axis=0)
+
+    # Use the model to predict the output-signals.
+    y_pred = model.predict(x)
+
+    # The output of the model is between 0 and 1.
+    # Do an inverse map to get it back to the scale
+    # of the original data-set.
+    y_pred_rescaled = y_scaler.inverse_transform(y_pred[0])
+
+    # For each output-signal.
+    for signal in range(len(target_names)):
+        # Get the output-signal predicted by the model.
+        signal_pred = y_pred_rescaled[:, signal]
+
+        # Get the true output-signal from the data-set.
+        signal_true = y_true[:, signal]
+
+        # Make the plotting-canvas bigger.
+        plt.figure(figsize=(15, 5))
+
+        # Plot and compare the two signals.
+        plt.plot(signal_true, label='true')
+        plt.plot(signal_pred*10, label='pred')
+
+        # Plot grey box for warmup-period.
+        p = plt.axvspan(0, warmup_steps, facecolor='black', alpha=0.15)
+
+        # Plot labels etc.
+        plt.ylabel(target_names[signal])
+        plt.legend()
+        plt.show()
+
+
+plot_comparison(start_idx=0, length=20000, train=False)
+
+
+def simulate_model_on_history(hist_length=100, length=100, train=False):
+    """
+    Parse historical data and use trained model to make buy/sell decisions
+
+    :param hist_length: length of previous data used to predict values
+    :param length: Sequence-length to process and plot.
+    :param train: Boolean whether to use training- or test-set.
+    """
+
+    buy_threshold = 0
+    sell_threshold = 0
+
+    # start in data at pos=hist_length (in order to have at leas the hist size before the first tested point)
+    # for each iteration before index reaches "length"
+        # select input sequence
+        # expand dims, then predict with model
+        # use last point of model output as predicted price change
+        # if buy: convert usdt value to btc value
+        # if sell: convert btc value to usdt
+
+    # if value is btc, convert to usdt with last known price
+    # print final usdt value
 
 
 pass
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,71 +1,96 @@
 __author__ = "Michel Tulane"
-#File created 24-JUL-2019
+# File created 24-JUL-2019
 
 
-# def data_summary(x_train, y_train, x_test, y_test):
-#     """Summarize current state of dataset"""
-#     print('Train images shape:', x_train.shape)
-#     print('Train labels shape:', y_train.shape)
-#     print('Test images shape:', x_test.shape)
-#     print('Test labels shape:', y_test.shape)
-#     print('Train labels:', y_train)
-#     print('Test labels:', y_test)
+def get_polo_data():
+    import pandas as pd
+    from datetime import datetime
+    import requests
+    import json
 
-from poloniex import Poloniex
-import pandas as pd
-from datetime import datetime
-import requests
-import json
+    start_epoch = 1496188800  # Wednesday 31 May 2017 00:00:00
+    one_month = 2592000  # 1 month unix time length
+    data_length_months = 29
+    data_period = 300  # 5 min
+    data_path = "../../data/5min_fetched/"
 
-START_EPOCH = 1494547200  # 2017 Mai 12th, 00:00:00 (GMT)
-ONE_MONTH = 2592000  # 1 month unix time length
-DATA_LENGTH_MONTHS = 28
-DATA_PERIOD = 300  # 5 min
-DATA_PATH = "../../data/5min_fetched/"
+    # pairs = ["USDT_BTC", "USDT_ETH", "USDT_XRP", "USDT_LTC"]
+    pairs = ["USDT_BTC"]
+    polo_public_url = "https://poloniex.com/public?"
+    cmd_return_chart_data = "returnChartData"
 
-PAIRS = ["USDT_BTC", "USDT_ETH", "USDT_XRP", "USDT_LTC"]
-POLO_PUBLIC_URL = "https://poloniex.com/public?"
-CMD_RETURN_CHART_DATA = "returnChartData"
+    for pair in pairs:
+        start = start_epoch
+        data = []
+        for month in range(data_length_months):
 
-for pair in PAIRS:
-    start = START_EPOCH
-    data = []
-    for month in range(DATA_LENGTH_MONTHS):
+            end = start + one_month - data_period
 
-        end = start + ONE_MONTH - DATA_PERIOD
+            print("Fetch currency pair {} for month {} out of {}".format(pair, month + 1, data_length_months))
 
-        print("Fetch currency pair {} for month {} out of {}".format(pair, month+1, DATA_LENGTH_MONTHS))
+            "Fetch currency pair {} for month {} out of {}".format(pair, month + 1, data_length_months)
+            reponse = requests.get(
+                url=(polo_public_url + "command={cmd}&currencyPair={curr_pair}&start={start}&end={end}&period={period}"
+                     .format(cmd=cmd_return_chart_data,
+                             curr_pair=pair,
+                             start=start,
+                             end=end,
+                             period=data_period)))
+            reponse.encoding = "utf-8"
+            temp = json.loads(reponse.content)
 
-        "Fetch currency pair {} for month {} out of {}".format(pair, month + 1, DATA_LENGTH_MONTHS)
+            # check if response is the correct length
+            if len(temp) != 8640:
+                print("wrong response size. Retrying with increased end epoch")
+                reponse = requests.get(
+                    url=(
+                                polo_public_url + "command={cmd}&currencyPair={curr_pair}&start={start}&end={end}&period={period}"
+                                .format(cmd=cmd_return_chart_data,
+                                        curr_pair=pair,
+                                        start=start,
+                                        end=end + data_period,
+                                        period=data_period)))
+                reponse.encoding = "utf-8"
+                temp = json.loads(reponse.content)[:-1]
+                print(temp[0])
+                print(temp[-1])
 
-        reponse = requests.get(
-            url=(POLO_PUBLIC_URL + "command={cmd}&currencyPair={curr_pair}&start={start}&end={end}&period={period}"
-                 .format(cmd=CMD_RETURN_CHART_DATA,
-                         curr_pair=pair,
-                         start=start,
-                         end=end,
-                         period=DATA_PERIOD)))
-        reponse.encoding = "utf-8"
+            data.extend(temp)
+            start = start + one_month
 
-        data.extend(json.loads(reponse.content))
-        start = start + ONE_MONTH
+        # generate datetime, day and time values from unix timestamp
+        print("Generating metadata and features...")
+        for i in data:
+            dtobj = datetime.fromtimestamp(i["date"])
+            i.update({"day": dtobj.strftime("%d %B, %Y")})
+            i.update({"time": dtobj.strftime("%H:%M:%S")})
+            i.update({"hour": int(dtobj.strftime("%H"))})
+            i.update({"weekday": int(dtobj.strftime("%w"))})
+            i.update({(pair + "_price_change_last_5min"): (i["close"] - i["open"])})
+            i.update({(pair + "_volatility"): (i["high"] - i["low"])})
+            i[(pair + "_volume")] = i.pop("volume")
+            i[(pair + "_high")] = i.pop("high")
+            i[(pair + "_low")] = i.pop("low")
+            i[(pair + "_open")] = i.pop("open")
+            i[(pair + "_close")] = i.pop("close")
 
-    # generate datetime, day and time values from unix timestamp
-    print("Generating metadata and features...")
-    for i in data:
-        dtobj = datetime.fromtimestamp(i["date"])
-        i.update({"day": dtobj.strftime("%d %B, %Y")})
-        i.update({"time": dtobj.strftime("%H:%M:%S")})
-        i.update({"hour": dtobj.strftime("%H")})
-        i.update({"weekday": dtobj.strftime("%w")})
-        i.update({(pair + "_price_change_last_5min"): (i["close"] - i["open"])})
-        i.update({(pair + "_volatility"): (i["high"] - i["low"])})
-        i[(pair + "_volume")] = i.pop("volume")
+        print("Generating prediction values")
+        for index, i in enumerate(data[:-3]):
+            i.update({(pair + "_expected_price_change_15min"): (data[index + 3][pair + "_close"] - i[pair + "_close"])})
+            i.update({(pair + "_expected_price_change_5min"): (data[index + 1][pair + "_close"] - i[pair + "_close"])})
 
-    print("Generating prediction values")
-    for index, i in enumerate(data[:-3]):
-        i.update({(pair + "_expected_price_change_15min"): (data[index + 3]["close"] - i["close"])})
+        # Convert to dataframe
+        data_df = pd.DataFrame(data)
 
-    #  save data to excel
-    print("Saving to excel...")
-    pd.DataFrame(data).to_excel(DATA_PATH + pair + ".xlsx")
+        # Drop last 3 rows
+        data_df = data_df.iloc[:-3]
+
+        # save data to excel
+        # print("Saving to excel...")
+        # data_df.to_excel(data_path + pair + ".xlsx", index=False)
+
+        print("Saving to feather...")
+        data_df.to_feather(data_path + pair + ".feather")
+
+
+
